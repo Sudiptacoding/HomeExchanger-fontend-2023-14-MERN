412c3d03d4f102fca241ce024099428398554bc7
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-08 18:31:33 +0600

    New link

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,19 +1,21 @@
 import React, { useContext, useEffect } from 'react';
+import { UserProvider } from '../context/AuthContext';
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://backend-eovuj1l7a-sudiptacoding.vercel.app',
+    // baseURL: 'https://backend-lmr9alq3n-sudiptacoding.vercel.app',
+    baseURL: 'https://backend-66p8f71fp-sudiptacoding.vercel.app',
     withCredentials: true
 });
 
 const useAxios = () => {
-
+    const { logOut } = useContext(UserProvider)
     useEffect(() => {
         instance.interceptors.response.use(function (response) {
             return response;
         }, function (error) {
             if (error.response.status === 401 || error.response.status === 403) {
-                console.log('no add')
+                logOut()
             }
         });
     }, [])
