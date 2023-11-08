23b0b42c184f9d94c724b18efc99ee0e9e75b9ff
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-08 14:26:43 +0600

    Contex api setup sucessfully

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -46,7 +46,6 @@ const Header = () => {
                                         isPending ? "pending" : isActive ? "font-medium text-blue-600 sm:py-6 dark:text-blue-500" : "font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500"
                                     }
 
-
                                     to='/' aria-current="page">Home</NavLink>
 
 
