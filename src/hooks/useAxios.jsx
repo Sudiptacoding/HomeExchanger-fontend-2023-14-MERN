889d68adceb77d20cd23658eb456b69b254eedd4
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-08 14:42:58 +0600

    Hook create with bech url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,9 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { UserProvider } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
-
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000',
@@ -12,14 +9,12 @@ const instance = axios.create({
 
 const useAxios = () => {
     const { logOut } = useContext(UserProvider)
-
     useEffect(() => {
         instance.interceptors.response.use(function (response) {
             return response;
         }, function (error) {
             if (error.response.status === 401 || error.response.status === 403) {
                 logOut()
-
             }
         });
     }, [])
