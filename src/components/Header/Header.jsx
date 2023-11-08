509c0e171ae1489395394b81a18f5961eabf2da3
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-08 14:28:01 +0600

    Navbar create sucessfully

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -52,7 +52,6 @@ const Header = () => {
                                     }
                                     to='/allservices'
                                 >Services</NavLink>
-
                                 {
                                     user && <div className="hs-dropdown z-50 [--strategy:static] sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
                                         <button type="button" className="flex items-center w-full font-medium text-gray-500 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 ">
@@ -61,7 +60,6 @@ const Header = () => {
                                                 <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
                                             </svg>
                                         </button>
-
                                         <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:left-0 before:w-full before:h-5">
                                             <NavLink to='/myservices'
                                                 className={({ isActive, isPending }) =>
@@ -77,7 +75,6 @@ const Header = () => {
                                                         <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round"></path>
                                                     </svg>
                                                 </button>
-
                                                 <div className="hs-dropdown-menu  transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-20 sm:mt-2 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute sm:border before:-right-5 before:top-0 before:h-full before:w-5 top-0 right-full !mx-[10px]">
                                                     <NavLink to='/mybookings'
                                                         className={({ isActive, isPending }) =>
