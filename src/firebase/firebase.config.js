04fa7580241d29bd50991272ad9cfe286dffef2b
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-12-03 17:58:48 +0600

    Home exchanger

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_apiKey,
+//     authDomain: import.meta.env.VITE_authDomain,
+//     databaseURL: import.meta.env.VITE_databaseURL,
+//     projectId: import.meta.env.VITE_projectId,
+//     storageBucket: import.meta.env.VITE_storageBucket,
+//     messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//     appId: import.meta.env.VITE_appId,
+// };
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain,
-    databaseURL: import.meta.env.VITE_databaseURL,
-    projectId: import.meta.env.VITE_projectId,
-    storageBucket: import.meta.env.VITE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_messagingSenderId,
-    appId: import.meta.env.VITE_appId,
+    apiKey: "AIzaSyBqe8D3zs2l5O4A8vqmTbzIwJQvyo8jU8A",
+    authDomain: "volentear-8e15a.firebaseapp.com",
+    projectId: "volentear-8e15a",
+    storageBucket: "volentear-8e15a.appspot.com",
+    messagingSenderId: "484994612394",
+    appId: "1:484994612394:web:0f68374270a38b51b564d3"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
